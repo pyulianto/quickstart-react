8c9c75de0d5151dba1eabcd68090e0f0816bcff1
pyulianto <primayulianto@gmail.com>
2024-09-03 22:19:34 +0800

    Update the AssistantId

--- src/components/ActiveCallDetail.jsx
+++ src/components/ActiveCallDetail.jsx
@@ -12,11 +12,12 @@ const ActiveCallDetail = ({ assistantIsSpeaking, volumeLevel, onEndCallClick }) 
           alignItems: "center",
           justifyContent: "center",
           padding: "15px",
-          border: "1px solid #ddd",
+          border: "1px solid #000",
           borderRadius: "8px",
           boxShadow: "0px 4px 8px rgba(0,0,0,0.1)",
           width: "400px",
           height: "200px",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
         }}
       >
         <AssistantSpeechIndicator isSpeaking={assistantIsSpeaking} />
